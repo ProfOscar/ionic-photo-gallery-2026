b7102c322711b552c04c5c8ac0040ad5c9a8c9da
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-04-22 12:49:31 +0200

    Iniziato salvataggio su filesystem dell'immagine appena scattata

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+
 import { Camera, CameraDirection } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoService {
-  public photos:UserPhoto[] = [];
-  
-  public async addNewToGallery(){
+  public photos: UserPhoto[] = [];
+
+  public async addNewToGallery() {
     // Scattiamo una foto
     const capturedPhoto = await Camera.takePhoto({
       cameraDirection: CameraDirection.Front,
@@ -21,7 +23,38 @@ export class PhotoService {
       webviewPath: capturedPhoto.webPath
     })
 
-    console.log(this.photos);
+    //console.log(this.photos);
+
+    const savedImageFile = await this.savePicture(capturedPhoto.webPath!);
+  }
+
+  private async savePicture(webPath: string) {
+    const response = await fetch(webPath!);
+    const blob = await response.blob();
+    const base64Data = (await this.convertBlobToBase64(blob)) as string;
+
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: webPath
+    }
+  }
+
+  private convertBlobToBase64(blob: Blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
   }
 }
 
