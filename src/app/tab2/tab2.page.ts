4dcf18920b538e92f6f22189396c24200e287547
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-05-06 12:27:00 +0200

    Aggiunto cancellazione immagine usando il plugin capacitor action-sheet; progetto concluso

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { camera } from 'ionicons/icons';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 
 @Component({
   selector: 'app-tab2',
@@ -24,5 +25,28 @@ export class Tab2Page {
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    console.log("Entra in showActionSheet");
+    const actionSheet = await ActionSheet.showActions({
+      title: 'Photos',
+      cancelable: true,
+      options: [
+        {
+          title: 'Delete',
+          style: ActionSheetButtonStyle.Destructive,
+          icon: 'trash',
+        },
+        {
+          title: 'Cancel',
+          icon: 'close',
+          style: ActionSheetButtonStyle.Cancel,
+        },
+      ],
+    });
+    if (actionSheet.index == 0) {
+      this.photoService.deletePhoto(photo, position);
+    }
   }
 }
